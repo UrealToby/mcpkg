d8891b325a8bd1aaaac1e9473fc9dc55620bbe6b
UrealToby <tobyprime0516@outlook.com>
2022-12-05 02:52:49 +0800

    Initial commit

--- mcpkg/package.h
+++ mcpkg/package.h
@@ -27,7 +27,7 @@ namespace mcpkg{
 
     };
 
-    /// 静态文件，比如Mod,材质,光影,只需要根据配置文件放入指定文件夹
+    /// 静态文件，比如 Mod,材质,光影,只需要根据配置文件放入指定文件夹
     /// 不存储文件，只存储配置
     class StaticPackage : public Package{
         // 是否解压
@@ -39,11 +39,11 @@ namespace mcpkg{
     /// 不存储被执行文件，只存储执行配置
     class ExecutablePackage: public Package{
         // 是否需要通过 java
-        bool byJava;
+        bool byJava{};
         //todo: 执行参数
         //todo: 存放路径
     };
-
+    /// 需要执行脚本文件，比如，整合包
     /// 不存储脚本文件，只存储脚本配置，其中包含脚本地址、执行配置、其他资源文件
     class ScriptRequiredPackage : public Package{
         std::string scriptPath;
